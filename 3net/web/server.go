78ced395552ecc3159d32d0ec6fb96bbbe6dfe77
Ismael Jiménez Mejías <isjim2326@uis.no>
2025-09-01 17:36:49 +0200

    1version server.go

--- 3net/web/server.go
+++ 3net/web/server.go
@@ -2,32 +2,83 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
 var httpAddr = flag.String("http", ":8080", "Listen address")
 
+// Server struct con estado (counter) y mutex para concurrencia
+type Server struct {
+	mu      sync.Mutex
+	counter int
+}
+
+// NewServer inicializa el Server con estado inicial
+func NewServer() *Server {
+	return &Server{
+		counter: 0,
+	}
+}
+
+// ServeHTTP implementa el http.Handler interface
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	
+	s.mu.Lock()
+	s.counter++
+	count := s.counter
+	s.mu.Unlock()
+
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprintln(w, "Hello World!")
+
+	case "/counter":
+		fmt.Fprintf(w, "counter: %d\n", count)
+
+	case "/fizzbuzz":
+		valStr := r.URL.Query().Get("value")
+		if valStr == "" {
+			fmt.Fprintln(w, "no value provided")
+			return
+		}
+
+		n, err := strconv.Atoi(valStr)
+		if err != nil {
+			fmt.Fprintln(w, "not an integer")
+			return
+		}
+
+		fmt.Fprintln(w, fizzbuzz(n))
+
+	case "/github":
+		http.Redirect(w, r, "http://www.github.com", http.StatusMovedPermanently)
+
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+// lógica FizzBuzz
+func fizzbuzz(n int) string {
+	switch {
+	case n%15 == 0:
+		return "fizzbuzz"
+	case n%3 == 0:
+		return "fizz"
+	case n%5 == 0:
+		return "buzz"
+	default:
+		return fmt.Sprintf("%d", n)
+	}
+}
+
 func main() {
 	flag.Parse()
 	server := NewServer()
+	log.Println("Server running on", *httpAddr)
 	log.Fatal(http.ListenAndServe(*httpAddr, server))
-}
-
-// Server implements the web server specification found at
-// lab2/README.md#web-server
-type Server struct {
-	// TODO(student): Add needed fields
-}
-
-// NewServer returns a new Server with all required internal state initialized.
-// NOTE: It should NOT start to listen on an HTTP endpoint.
-func NewServer() *Server {
-	s := &Server{}
-	// TODO(student): Implement
-	return s
-}
-
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// TODO(student): Implement
 }
